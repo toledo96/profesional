15b8e24a32bb0fb3578fe346453289db402b8735
Andres <atm1996.ast@gmail.com>
2026-04-30 20:41:26 -0600

    register-server agregado

---
+++ BackEnd/pedidos/Pedidos/register-service/src/main/java/com/techi/microservices/register_service/RegisterServiceApplication.java
@@ -0,0 +1,15 @@
+package com.techi.microservices.register_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterServiceApplication.class, args);
+	}
+
+}
